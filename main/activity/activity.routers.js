2181e9b2410c40876dd973dd33b035a1cbedaa95
Jesika Neisy Vega Ruiz <jesikanvr@gmail.com>
2024-05-20 14:42:14 -0400

    ability_controller and login_controller

--- main/activity/activity.routers.js
+++ main/activity/activity.routers.js
@@ -2,8 +2,10 @@ const { Router } = require("express");
 const { check } = require("express-validator");
 
 const { Post_ACTIVITY_ID } = require("./activity.controllers");
+const { POST_ACTIVITY_FOR_OBJECTIVE } = require("./activity.controllers");
 const { INSERT_ACTIVITY } = require("./activity.controllers");
 const { UPDATE_ACTIVITY } = require("./activity.controllers");
+const { DELETE_ACTIVITY } = require("./activity.controllers");
 const { validateFields } = require("../../middlewares/validate-fields");
 
 
@@ -18,6 +20,16 @@ router.post(
   ],
   
   Post_ACTIVITY_ID
+);
+router.post(
+  "/id_obj",
+  [ 
+    //check("id_stage", "The id is invalid").isUUID(4),
+    //check("id_ability", "The id is invalid").isUUID(4),
+    validateFields 
+  ],
+  
+  POST_ACTIVITY_FOR_OBJECTIVE
 );
 
 router.post(
@@ -34,6 +46,12 @@ router.put(
   "/updateActivity",
   [ validateFields ],
   UPDATE_ACTIVITY
+);
+
+router.delete(
+  "/deleteActivity",
+  [ validateFields ],
+  DELETE_ACTIVITY
 );
 
 module.exports = router;
